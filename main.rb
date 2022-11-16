f6d835033ae33dd4293ce1ccbe4314f5373918a9
Евгений Поляков <epoliakov@gmail.com>
2022-11-16 13:20:56 +0300

    add gemfile and rakefile

--- main.rb
+++ main.rb
@@ -2,6 +2,13 @@ require 'sinatra'
 require 'rubygems'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db"
+class Client < ActiveRecord::Base
+
+end
+
 
 get '/' do
     erb :index
