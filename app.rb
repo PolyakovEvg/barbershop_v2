46c0cb5e0efb02604897f4665a3b9f5e7a08fcfb
Евгений Поляков <epoliakov@gmail.com>
2022-11-17 15:45:17 +0300

    add jquery plugins and visit page with save data

--- app.rb
+++ app.rb
@@ -13,8 +13,28 @@ end
 class Barber < ActiveRecord::Base
 end
 
-
+before do
+    @barbers = Barber.order "created_at DESC"
+end
 get '/' do
-    @barbers = Barber.all
     erb :index
+end
+
+get '/about' do
+    erb :about
+end
+
+get '/visit' do
+    erb :visit
+end
+
+post '/visit' do
+    @user_name = params[:user_name]
+    @user_phone = params[:user_phone]
+    @date = params[:user_visit_time]
+    @master = params[:select]
+    @color = params[:colorpicker]
+    user = Client.create(:name => @username, :phone =>@user_phone, :datestamp => @date, :barber => @master, :color =>@color)
+    erb :visit
+
 end
